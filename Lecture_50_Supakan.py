a1a762e388cde07d59fb477d54da61fb36b6d272
MinIzL <minilekz@gmail.com>
2022-12-23 14:23:12 +0700

    Create Lecture_50_Supakan.py

---
+++ Lecture_50_Supakan.py
@@ -0,0 +1,8 @@
+def addNumber(x,y):
+    print("x+y = ",x+y)
+    print("x-y = ",x-y)
+    print("x*y = ",x*y)
+    print("x/y = ",x/y)
+x = int(input("x Number: "))
+y = int(input("y Number: "))
+addNumber(x,y)
